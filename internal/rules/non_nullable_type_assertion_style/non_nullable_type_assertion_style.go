e95aada75d6d6691280c7409879dcf0af9cb084b
auvred <aauvred@gmail.com>
2025-04-06 18:56:07 +0300

    implement non-nullable-type-assertion-style

---
+++ internal/rules/non_nullable_type_assertion_style/non_nullable_type_assertion_style.go
@@ -0,0 +1,105 @@
+package non_nullable_type_assertion_style
+
+import (
+	"github.com/microsoft/typescript-go/shim/ast"
+	"github.com/microsoft/typescript-go/shim/checker"
+	"github.com/microsoft/typescript-go/shim/core"
+	"github.com/typescript-eslint/tsgolint/internal/rule"
+	"github.com/typescript-eslint/tsgolint/internal/utils"
+)
+
+func buildPreferNonNullAssertionMessage() rule.RuleMessage {
+	return rule.RuleMessage{
+		Id:          "preferNonNullAssertion",
+		Description: "Use a ! assertion to more succinctly remove null and undefined from the type.",
+	}
+}
+
+var NonNullableTypeAssertionStyleRule = rule.Rule{
+	Name: "non-nullable-type-assertion-style",
+	Run: func(ctx rule.RuleContext, options any) rule.RuleListeners {
+		getTypesIfNotLoose := func(node *ast.Node) []*checker.Type {
+			t := ctx.TypeChecker.GetTypeAtLocation(node)
+			if utils.IsTypeFlagSet(t, checker.TypeFlagsAny|checker.TypeFlagsUnknown) {
+				return nil
+			}
+			return utils.UnionTypeParts(t)
+		}
+
+		couldBeNullable := func(t *checker.Type) bool {
+			if utils.IsTypeParameter(t) {
+				t = checker.Checker_getBaseConstraintOfType(ctx.TypeChecker, t)
+				if t == nil {
+					return true
+				}
+			}
+			for _, p := range utils.UnionTypeParts(t) {
+				if utils.IsTypeFlagSet(p, checker.TypeFlagsNullable) {
+					return true
+				}
+			}
+			return false
+		}
+
+		checkAssertion := func(node *ast.Node) {
+			if ast.IsConstAssertion(node) {
+				return
+			}
+
+			expression := node.Expression()
+			originalTypes := getTypesIfNotLoose(expression)
+			if originalTypes == nil {
+				return
+			}
+
+			typeAnnotation := ast.GetAssertedTypeNode(node)
+			assertedTypes := getTypesIfNotLoose(typeAnnotation)
+			if assertedTypes == nil {
+				return
+			}
+
+			nonNullableOriginalType := utils.NewSetWithSizeHint[*checker.Type](len(originalTypes))
+			for _, t := range originalTypes {
+				if !utils.IsTypeFlagSet(t, checker.TypeFlagsNullable) {
+					nonNullableOriginalType.Add(t)
+				}
+			}
+			if nonNullableOriginalType.Len() == len(originalTypes) {
+				return
+			}
+
+			assertedTypesSet := utils.NewSetWithSizeHint[*checker.Type](len(assertedTypes))
+			for _, t := range assertedTypes {
+				if couldBeNullable(t) || !nonNullableOriginalType.Has(t) {
+					return
+				}
+				assertedTypesSet.Add(t)
+			}
+
+			for originalType := range nonNullableOriginalType.Keys() {
+				if !assertedTypesSet.Has(originalType) {
+					return
+				}
+			}
+
+			higherPrecedenceThanUnary := ast.GetExpressionPrecedence(expression) > ast.OperatorPrecedenceUnary
+
+			var removeRange core.TextRange
+			if ast.IsAssertionExpression(node) {
+				removeRange = node.Loc.WithPos(expression.End())
+			} else {
+				removeRange = node.Loc.WithEnd(expression.Pos())
+			}
+			if higherPrecedenceThanUnary {
+				ctx.ReportNodeWithFixes(node, buildPreferNonNullAssertionMessage(), rule.RuleFixRemoveRange(removeRange), rule.RuleFixInsertAfter(expression, "!"))
+			} else {
+				ctx.ReportNodeWithFixes(node, buildPreferNonNullAssertionMessage(), rule.RuleFixRemoveRange(removeRange), rule.RuleFixInsertBefore(ctx.SourceFile, expression, "("), rule.RuleFixInsertAfter(expression, ")!"))
+			}
+		}
+
+		return rule.RuleListeners{
+			ast.KindAsExpression:            checkAssertion,
+			ast.KindTypeAssertionExpression: checkAssertion,
+		}
+	},
+}
---
+++ internal/rules/non_nullable_type_assertion_style/non_nullable_type_assertion_style_test.go
@@ -0,0 +1,302 @@
+package non_nullable_type_assertion_style
+
+import (
+	"testing"
+
+	"github.com/typescript-eslint/tsgolint/internal/rule_tester"
+	"github.com/typescript-eslint/tsgolint/internal/rules/fixtures"
+)
+
+func TestNonNullableTypeAssertionStyleRule(t *testing.T) {
+	rule_tester.RunRuleTester(fixtures.GetRootDir(), "tsconfig.json", t, &NonNullableTypeAssertionStyleRule, []rule_tester.ValidTestCase{
+		{Code: `
+declare const original: number | string;
+const cast = original as string;
+    `},
+		{Code: `
+declare const original: number | undefined;
+const cast = original as string | number | undefined;
+    `},
+		{Code: `
+declare const original: number | any;
+const cast = original as string | number | undefined;
+    `},
+		{Code: `
+declare const original: number | undefined;
+const cast = original as any;
+    `},
+		{Code: `
+declare const original: number | null | undefined;
+const cast = original as number | null;
+    `},
+		{Code: `
+type Type = { value: string };
+declare const original: Type | number;
+const cast = original as Type;
+    `},
+		{Code: `
+type T = string;
+declare const x: T | number;
+
+const y = x as NonNullable<T>;
+    `},
+		{Code: `
+type T = string | null;
+declare const x: T | number;
+
+const y = x as NonNullable<T>;
+    `},
+		{Code: `
+const foo = [] as const;
+    `},
+		{Code: `
+const x = 1 as 1;
+    `},
+		{Code: `
+declare function foo<T = any>(): T;
+const bar = foo() as number;
+    `},
+	}, []rule_tester.InvalidTestCase{
+		{
+			Code: `
+declare const maybe: string | undefined;
+const bar = maybe as string;
+      `,
+			Output: []string{`
+declare const maybe: string | undefined;
+const bar = maybe!;
+      `,
+			},
+			Errors: []rule_tester.InvalidTestCaseError{
+				{
+					MessageId: "preferNonNullAssertion",
+					Line:      3,
+					Column:    13,
+				},
+			},
+		},
+		{
+			Code: `
+declare const maybe: string | null;
+const bar = maybe as string;
+      `,
+			Output: []string{`
+declare const maybe: string | null;
+const bar = maybe!;
+      `,
+			},
+			Errors: []rule_tester.InvalidTestCaseError{
+				{
+					MessageId: "preferNonNullAssertion",
+					Line:      3,
+					Column:    13,
+				},
+			},
+		},
+		{
+			Code: `
+declare const maybe: string | null | undefined;
+const bar = maybe as string;
+      `,
+			Output: []string{`
+declare const maybe: string | null | undefined;
+const bar = maybe!;
+      `,
+			},
+			Errors: []rule_tester.InvalidTestCaseError{
+				{
+					MessageId: "preferNonNullAssertion",
+					Line:      3,
+					Column:    13,
+				},
+			},
+		},
+		{
+			Code: `
+type Type = { value: string };
+declare const maybe: Type | undefined;
+const bar = maybe as Type;
+      `,
+			Output: []string{`
+type Type = { value: string };
+declare const maybe: Type | undefined;
+const bar = maybe!;
+      `,
+			},
+			Errors: []rule_tester.InvalidTestCaseError{
+				{
+					MessageId: "preferNonNullAssertion",
+					Line:      4,
+					Column:    13,
+				},
+			},
+		},
+		{
+			Code: `
+interface Interface {
+  value: string;
+}
+declare const maybe: Interface | undefined;
+const bar = maybe as Interface;
+      `,
+			Output: []string{`
+interface Interface {
+  value: string;
+}
+declare const maybe: Interface | undefined;
+const bar = maybe!;
+      `,
+			},
+			Errors: []rule_tester.InvalidTestCaseError{
+				{
+					MessageId: "preferNonNullAssertion",
+					Line:      6,
+					Column:    13,
+				},
+			},
+		},
+		{
+			Code: `
+type T = string | null;
+declare const x: T;
+
+const y = x as NonNullable<T>;
+      `,
+			Output: []string{`
+type T = string | null;
+declare const x: T;
+
+const y = x!;
+      `,
+			},
+			Errors: []rule_tester.InvalidTestCaseError{
+				{
+					MessageId: "preferNonNullAssertion",
+					Line:      5,
+					Column:    11,
+				},
+			},
+		},
+		{
+			Code: `
+type T = string | null | undefined;
+declare const x: T;
+
+const y = x as NonNullable<T>;
+      `,
+			Output: []string{`
+type T = string | null | undefined;
+declare const x: T;
+
+const y = x!;
+      `,
+			},
+			Errors: []rule_tester.InvalidTestCaseError{
+				{
+					MessageId: "preferNonNullAssertion",
+					Line:      5,
+					Column:    11,
+				},
+			},
+		},
+		{
+			Code: `
+declare function nullablePromise(): Promise<string | null>;
+
+async function fn(): Promise<string> {
+  return (await nullablePromise()) as string;
+}
+      `,
+			Output: []string{`
+declare function nullablePromise(): Promise<string | null>;
+
+async function fn(): Promise<string> {
+  return (await nullablePromise())!;
+}
+      `,
+			},
+			Errors: []rule_tester.InvalidTestCaseError{
+				{
+					MessageId: "preferNonNullAssertion",
+					Line:      5,
+					Column:    10,
+				},
+			},
+		},
+		{
+			Code: `
+declare const a: string | null;
+
+const b = (a || undefined) as string;
+      `,
+			Output: []string{`
+declare const a: string | null;
+
+const b = (a || undefined)!;
+      `,
+			},
+			Errors: []rule_tester.InvalidTestCaseError{
+				{
+					MessageId: "preferNonNullAssertion",
+					Line:      4,
+					Column:    11,
+				},
+			},
+		},
+	})
+}
+
+func TestNonNullableTypeAssertionStyleRule_noUncheckedIndexedAccess(t *testing.T) {
+	rule_tester.RunRuleTester(fixtures.GetRootDir(), "tsconfig.noUncheckedIndexedAccess.json", t, &NonNullableTypeAssertionStyleRule, []rule_tester.ValidTestCase{
+		{Code: `
+function first<T>(array: ArrayLike<T>): T | null {
+  return array.length > 0 ? (array[0] as T) : null;
+}
+      `},
+		{Code: `
+function first<T extends string | null>(array: ArrayLike<T>): T | null {
+  return array.length > 0 ? (array[0] as T) : null;
+}
+      `},
+		{Code: `
+function first<T extends string | undefined>(array: ArrayLike<T>): T | null {
+  return array.length > 0 ? (array[0] as T) : null;
+}
+      `},
+		{Code: `
+function first<T extends string | null | undefined>(
+  array: ArrayLike<T>,
+): T | null {
+  return array.length > 0 ? (array[0] as T) : null;
+}
+      `},
+		{Code: `
+type A = 'a' | 'A';
+type B = 'b' | 'B';
+function first<T extends A | B | null>(array: ArrayLike<T>): T | null {
+  return array.length > 0 ? (array[0] as T) : null;
+}
+      `},
+	}, []rule_tester.InvalidTestCase{
+		{
+			Code: `
+function first<T extends string | number>(array: ArrayLike<T>): T | null {
+  return array.length > 0 ? (array[0] as T) : null;
+}
+        `,
+			Output: []string{`
+function first<T extends string | number>(array: ArrayLike<T>): T | null {
+  return array.length > 0 ? (array[0]!) : null;
+}
+        `,
+			},
+			Errors: []rule_tester.InvalidTestCaseError{
+				{
+					MessageId: "preferNonNullAssertion",
+					Line:      3,
+					Column:    30,
+				},
+			},
+		},
+	})
+}
