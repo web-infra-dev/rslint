6eead35d4aaeed3dd35f217b797314dc1c84d6aa
auvred <aauvred@gmail.com>
2025-04-06 18:56:09 +0300

    implement require-array-sort-compare

---
+++ internal/rules/require_array_sort_compare/require_array_sort_compare.go
@@ -0,0 +1,66 @@
+package require_array_sort_compare
+
+import (
+	"github.com/microsoft/typescript-go/shim/ast"
+	"github.com/microsoft/typescript-go/shim/checker"
+	"github.com/typescript-eslint/tsgolint/internal/rule"
+	"github.com/typescript-eslint/tsgolint/internal/utils"
+)
+
+func buildRequireCompareMessage() rule.RuleMessage {
+	return rule.RuleMessage{
+		Id:          "requireCompare",
+		Description: "Require 'compare' argument.",
+	}
+}
+
+type RequireArraySortCompareOptions struct {
+	IgnoreStringArrays *bool
+}
+
+var RequireArraySortCompareRule = rule.Rule{
+	Name: "require-array-sort-compare",
+	Run: func(ctx rule.RuleContext, options any) rule.RuleListeners {
+		opts, ok := options.(RequireArraySortCompareOptions)
+		if !ok {
+			opts = RequireArraySortCompareOptions{}
+		}
+		if opts.IgnoreStringArrays == nil {
+			opts.IgnoreStringArrays = utils.Ref(true)
+		}
+
+		return rule.RuleListeners{
+			ast.KindCallExpression: func(node *ast.Node) {
+				expr := node.AsCallExpression()
+				if len(expr.Arguments.Nodes) != 0 {
+					return
+				}
+				callee := expr.Expression
+
+				if !ast.IsAccessExpression(callee) {
+					return
+				}
+
+				if propertyName, found := checker.Checker_getAccessedPropertyName(ctx.TypeChecker, callee); !found || (propertyName != "sort" && propertyName != "toSorted") {
+					return
+				}
+
+				calleeObjType := utils.GetConstrainedTypeAtLocation(ctx.TypeChecker, callee.Expression())
+
+				if *opts.IgnoreStringArrays && checker.Checker_isArrayOrTupleType(ctx.TypeChecker, calleeObjType) {
+					if utils.Every(checker.Checker_getTypeArguments(ctx.TypeChecker, calleeObjType), func(t *checker.Type) bool {
+						return utils.IsTypeFlagSet(t, checker.TypeFlagsString)
+					}) {
+						return
+					}
+				}
+
+				if utils.Every(utils.UnionTypeParts(calleeObjType), func(t *checker.Type) bool {
+					return checker.Checker_isArrayOrTupleType(ctx.TypeChecker, t)
+				}) {
+					ctx.ReportNode(node, buildRequireCompareMessage())
+				}
+			},
+		}
+	},
+}
