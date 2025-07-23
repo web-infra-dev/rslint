a8f95ec86db1317ba4c87b7bb88f519286760867
hardfist <yangjianzju@gmail.com>
2025-07-23 10:55:26 +0800

    chore: use rslint in rslint (#41)

---
+++ packages/rslint/bin/rslint.cjs
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+const path = require('node:path');
+const binPath = path.resolve(__dirname, './rslint');
+require('child_process').execFileSync(binPath, process.argv.slice(2), {
+  stdio: 'inherit',
+});
