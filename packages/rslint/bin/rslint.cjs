53ed4b0abaabf149038dd86d7791f8456155175c
hardfist <yangjianzju@gmail.com>
2025-07-23 20:56:38 +0800

    chore: polish color

--- packages/rslint/bin/rslint.cjs
+++ packages/rslint/bin/rslint.cjs
@@ -1,7 +1,11 @@
 #!/usr/bin/env node
 const path = require('node:path');
 const os = require('node:os');
+const fs = require('node:fs');
 function getBinPath() {
+  if (fs.existsSync(path.resolve(__dirname, './rslint'))) {
+    return path.resolve(__dirname, './rslint');
+  }
   let platformKey = `${process.platform}-${os.arch()}`;
   return path.resolve(__dirname, `./rslint-${platformKey}`);
 
