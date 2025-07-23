2832facf158819d36b2a0226245b4ef0d98e264b
hardfist <yangjianzju@gmail.com>
2025-07-23 11:56:08 +0800

    chore: remove unrelated info

--- benchmarks/clone-projects.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env -S bash -euxo pipefail
-
-git clone --single-branch --depth 1 --branch 1.99.0 https://github.com/microsoft/vscode
-
-git clone -c core.longpaths=true --single-branch --depth 1 --branch v5.8.2 https://github.com/microsoft/typescript
-
-git clone --single-branch --depth 1 --branch 0.3.22 https://github.com/typeorm/typeorm
-
-git clone --single-branch --depth 1 --branch v3.5.13 https://github.com/vuejs/core vuejs
