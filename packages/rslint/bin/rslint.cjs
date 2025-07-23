708814230c2fb5faea5907f376b1479f23961b92
hardfist <yangjianzju@gmail.com>
2025-07-23 20:25:50 +0800

    chore: support publish to mac & linux

--- packages/rslint/bin/rslint.cjs
+++ packages/rslint/bin/rslint.cjs
@@ -1,6 +1,16 @@
 #!/usr/bin/env node
 const path = require('node:path');
-const binPath = path.resolve(__dirname, './rslint');
-require('child_process').execFileSync(binPath, process.argv.slice(2), {
-  stdio: 'inherit',
-});
+const os = require('node:os');
+function getBinPath() {
+  let platformKey = `${process.platform}-${os.arch()}`;
+  return path.resolve(__dirname, `./rslint-${platformKey}`);
+
+}
+function main() {
+  const binPath = getBinPath();
+  require('child_process').execFileSync(binPath, process.argv.slice(2), {
+    stdio: 'inherit',
+  });
+}
+main();
+
