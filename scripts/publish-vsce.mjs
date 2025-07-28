5e023b180eb4505f41878b75ce5e649a45322c50
hardfist <yangjianzju@gmail.com>
2025-07-28 18:56:30 +0800

    chore: support publish vsce to multi platform (#91)

---
+++ scripts/publish-vsce.mjs
@@ -0,0 +1,30 @@
+#!/usr/bin/env zx
+import fs from 'fs';
+
+$.verbose = true;
+async function publish_all() {
+  const version = JSON.parse(
+    fs.readFileSync('./packages/vscode-extension/package.json', 'utf-8'),
+  ).version;
+  await $`pnpm run --filter @rslint/core build`;
+  await $`pnpm run --filter rslint build`;
+  const platforms = [
+    { os: 'darwin', arch: 'amd64', 'node-arch': 'x64' },
+    { os: 'darwin', arch: 'arm64', 'node-arch': 'arm64' },
+    { os: 'linux', arch: 'amd64', 'node-arch': 'x64' },
+    { os: 'linux', arch: 'arm64', 'node-arch': 'arm64' },
+    { os: 'windows', arch: 'amd64', 'node-arch': 'x64', 'node-os': 'win32' },
+    { os: 'windows', arch: 'arm64', 'node-arch': 'arm64', 'node-os': 'win32' },
+  ];
+  for (const platform of platforms) {
+    console.log(`Start Publishing for ${platform.os}-${platform.arch}`);
+    await $`rm -rf ./packages/vscode-extension/dist/rslint && rm -rf ./packages/vscode-extension/dist/rslint.exe`;
+    await $`GOOS=${platform.os} GOARCH=${platform.arch} go build -o ./packages/vscode-extension/dist ./cmd/rslint`;
+    const os = platform['node-os'] || platform.os;
+    const arch = platform['node-arch'] || platform.arch;
+    await $`cd packages/vscode-extension && vsce package --target ${os}-${arch}`;
+    await $`cd packages/vscode-extension && vsce publish --packagePath ./rslint-${os}-${arch}-${version}.vsix `;
+    console.log(`Finish Publishing for ${os}-${arch}`);
+  }
+}
+publish_all();
