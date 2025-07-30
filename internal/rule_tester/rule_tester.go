93fa8f6162ac0b3c52d1ccd169d0c9d6def84b7b
hardfist <yangjianzju@gmail.com>
2025-07-30 16:31:35 +0800

    chore: run tests case in parallel

--- internal/rule_tester/rule_tester.go
+++ internal/rule_tester/rule_tester.go
@@ -105,6 +105,8 @@ func RunRuleTester(rootDir string, tsconfigPath string, t *testing.T, r *rule.Ru
 
 	for i, testCase := range validTestCases {
 		t.Run("valid-"+strconv.Itoa(i), func(t *testing.T) {
+			t.Parallel() // Enable parallel execution for this test case
+			
 			if (onlyMode && !testCase.Only) || testCase.Skip {
 				t.SkipNow()
 			}
@@ -123,6 +125,8 @@ func RunRuleTester(rootDir string, tsconfigPath string, t *testing.T, r *rule.Ru
 
 	for i, testCase := range invalidTestCases {
 		t.Run("invalid-"+strconv.Itoa(i), func(t *testing.T) {
+			t.Parallel() // Enable parallel execution for this test case
+			
 			if (onlyMode && !testCase.Only) || testCase.Skip {
 				t.SkipNow()
 			}
