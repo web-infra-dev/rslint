cddbe8a4f2871eeb78e0cfab90035a2f1cc0306f
ScriptedAlchemy <zackaryjackson@bytedance.com>
2025-08-05 17:58:07 -0700

    fix: disable no-empty-function rule for utils mock class
    The Linter class in packages/utils/index.ts is a stub/mock implementation
    for TypeScript ESLint compatibility. The empty methods are intentional
    placeholders and should be allowed.

--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -9,9 +9,13 @@ export namespace TSESTree {
   export type Node = any;
 }
 export namespace TSESLint {
+  // eslint-disable-next-line @typescript-eslint/no-empty-function
   export class Linter {
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     constructor(options: { configType: string }) {}
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     defineParser(name: string, parser: any) {}
+    // eslint-disable-next-line @typescript-eslint/no-empty-function
     verifyAndFix(code: string, options: any, options2: any): any {}
   }
 }
