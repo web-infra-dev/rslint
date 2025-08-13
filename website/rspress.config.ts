0d246674e9f1a263b7325190438e22c7f4779090
hardfist <yangjianzju@gmail.com>
2025-08-14 02:02:39 +0800

    chore: add rules page (#259)

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from '@rspress/core';
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
   title: 'Rslint',
-  icon: 'https://assets.rspack.rs/rslint/rslint-banner.png',
+  icon: 'https://assets.rspack.rs/rslint/rslint-logo.svg',
   logo: {
     light: 'https://assets.rspack.rs/rslint/rslint-logo.svg',
     dark: 'https://assets.rspack.rs/rslint/rslint-logo.svg',
