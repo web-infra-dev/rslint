b16c17e9e917740cbfada95314d565cb5a2b088f
hardfist <yangjianzju@gmail.com>
2025-08-14 11:13:59 +0800

    chore: update website style (#260)

--- website/rspress.config.ts
+++ website/rspress.config.ts
@@ -1,5 +1,14 @@
 import * as path from 'node:path';
+import { pluginSass } from '@rsbuild/plugin-sass';
 import { defineConfig } from '@rspress/core';
+import { pluginLlms } from '@rspress/plugin-llms';
+import { pluginGoogleAnalytics } from 'rsbuild-plugin-google-analytics';
+import { pluginOpenGraph } from 'rsbuild-plugin-open-graph';
+import { pluginFontOpenSans } from 'rspress-plugin-font-open-sans';
+import pluginSitemap from 'rspress-plugin-sitemap';
+
+const siteUrl = 'https://rslint.rs';
+const description = 'The Rspack-based testing framework';
 
 export default defineConfig({
   root: path.join(__dirname, 'docs'),
@@ -9,6 +18,23 @@ export default defineConfig({
     light: 'https://assets.rspack.rs/rslint/rslint-logo.svg',
     dark: 'https://assets.rspack.rs/rslint/rslint-logo.svg',
   },
+  logoText: 'Rslint',
+  search: {
+    codeBlocks: true,
+  },
+  lang: 'en',
+  locales: [
+    {
+      lang: 'en',
+      label: 'English',
+      description,
+    },
+  ],
+  route: {
+    cleanUrls: true,
+    // exclude document fragments from routes
+    exclude: ['**/zh/shared/**', '**/en/shared/**', './theme'],
+  },
   themeConfig: {
     socialLinks: [
       {
@@ -16,6 +42,43 @@ export default defineConfig({
         mode: 'link',
         content: 'https://github.com/web-infra-dev/rslint',
       },
+      {
+        icon: 'x',
+        mode: 'link',
+        content: 'https://twitter.com/rspack_dev',
+      },
+      {
+        icon: 'discord',
+        mode: 'link',
+        content: 'https://discord.gg/XsaKEEk4mW',
+      },
+    ],
+  },
+  plugins: [
+    pluginFontOpenSans(),
+    pluginSitemap({
+      domain: siteUrl,
+    }),
+    pluginLlms(),
+  ],
+  builderConfig: {
+    plugins: [
+      pluginSass(),
+      pluginGoogleAnalytics({
+        // cspell:disable-next-line
+        id: 'G-9WKFF5YJXQ',
+      }),
+      pluginOpenGraph({
+        title: 'Rslint',
+        type: 'website',
+        url: siteUrl,
+        image: 'https://assets.rspack.rs/rslint/rslint-logo.svg',
+        description,
+        twitter: {
+          site: '@rspack_dev',
+          card: 'summary_large_image',
+        },
+      }),
     ],
   },
 });
