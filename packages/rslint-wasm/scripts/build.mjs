7e99eb4a90e952d78927e9313733557d03fe3bfe
hardfist <yangjianzju@gmail.com>
2025-08-27 04:42:04 +0800

    chore: polish rslint page

--- packages/rslint-wasm/scripts/build.mjs
+++ packages/rslint-wasm/scripts/build.mjs
@@ -1,7 +1,7 @@
 import esbuild from 'esbuild';
 import fs from 'fs/promises';
 import path from 'path';
-import {polyfillNode} from 'esbuild-plugin-polyfill-node';
+import { polyfillNode } from 'esbuild-plugin-polyfill-node';
 import { fileURLToPath } from 'url';
 
 async function main() {
@@ -9,20 +9,23 @@ async function main() {
   await buildBrowser();
 }
 async function buildWorker() {
- await esbuild.build({
+  await esbuild.build({
     entryPoints: ['./src/worker.ts'],
     bundle: true,
     outfile: './dist/worker.js',
     platform: 'browser',
     target: 'es2020',
-    write:true,
+    write: true,
     format: 'iife',
     sourcemap: 'inline',
-    plugins: [ polyfillNode()]
+    plugins: [polyfillNode()],
   });
 }
 async function buildBrowser() {
-  const WEB_WORKER_SOURCE_CODE = await fs.readFile(path.resolve(import.meta.dirname, '../dist/worker.js'), 'utf8');
+  const WEB_WORKER_SOURCE_CODE = await fs.readFile(
+    path.resolve(import.meta.dirname, '../dist/worker.js'),
+    'utf8',
+  );
   await esbuild.build({
     entryPoints: ['./src/browser.ts'],
     outfile: './dist/index.mjs',
