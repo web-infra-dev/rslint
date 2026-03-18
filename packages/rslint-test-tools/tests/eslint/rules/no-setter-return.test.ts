94f164cd47e5e2f5a5d50780b89f0d4f27ac26ff
fansenze <151665030@qq.com>
2026-03-18 10:58:56 +0800

    feat: port rule no-setter-return

---
+++ packages/rslint-test-tools/tests/eslint/rules/no-setter-return.test.ts
@@ -0,0 +1,20 @@
+import { RuleTester } from '../rule-tester';
+
+const ruleTester = new RuleTester();
+
+ruleTester.run('no-setter-return', {
+  valid: [
+    'var foo = { set a(val) { val = 1; } };',
+    'class A { set a(val) { return; } }',
+  ],
+  invalid: [
+    {
+      code: 'var foo = { set a(val) { return 1; } };',
+      errors: [{ messageId: 'returnsValue' }],
+    },
+    {
+      code: 'class A { set a(val) { return val; } }',
+      errors: [{ messageId: 'returnsValue' }],
+    },
+  ],
+});
