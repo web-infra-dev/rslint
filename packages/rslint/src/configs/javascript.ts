077b1e1415ec9eeac5caf23d545a0f51a76a12cf
senze.fan <151665030@qq.com>
2026-04-07 13:20:39 +0800

    feat: enable newly ported rules in javascript recommended config (#598)

--- packages/rslint/src/configs/javascript.ts
+++ packages/rslint/src/configs/javascript.ts
@@ -10,34 +10,34 @@ const recommended: RslintConfigEntry = {
     // 'no-delete-var': 'error', // not implemented
     // 'no-dupe-class-members': 'error', // not implemented
     // 'no-dupe-else-if': 'error', // not implemented
-    // 'no-empty-character-class': 'error', // not implemented
+    'no-empty-character-class': 'error',
     // 'no-empty-static-block': 'error', // not implemented
-    // 'no-ex-assign': 'error', // not implemented
+    'no-ex-assign': 'error',
     // 'no-extra-boolean-cast': 'error', // not implemented
     // 'no-fallthrough': 'error', // not implemented
-    // 'no-func-assign': 'error', // not implemented
-    // 'no-global-assign': 'error', // not implemented
-    // 'no-import-assign': 'error', // not implemented
-    // 'no-invalid-regexp': 'error', // not implemented
+    'no-func-assign': 'error',
+    'no-global-assign': 'error',
+    'no-import-assign': 'error',
+    'no-invalid-regexp': 'error',
     // 'no-irregular-whitespace': 'error', // not implemented
     // 'no-misleading-character-class': 'error', // not implemented
     // 'no-new-native-nonconstructor': 'error', // not implemented
     // 'no-new-symbol': 'error', // not implemented (deprecated, use no-new-native-nonconstructor)
     // 'no-nonoctal-decimal-escape': 'error', // not implemented
-    // 'no-obj-calls': 'error', // not implemented
+    'no-obj-calls': 'error',
     // 'no-octal': 'error', // not implemented
     // 'no-prototype-builtins': 'error', // not implemented
     // 'no-redeclare': 'error', // not implemented
     // 'no-regex-spaces': 'error', // not implemented
-    // 'no-self-assign': 'error', // not implemented
-    // 'no-setter-return': 'error', // not implemented
+    'no-self-assign': 'error',
+    'no-setter-return': 'error',
     // 'no-shadow-restricted-names': 'error', // not implemented
-    // 'no-this-before-super': 'error', // not implemented
-    // 'no-undef': 'error', // not implemented
+    'no-this-before-super': 'error',
+    'no-undef': 'error',
     // 'no-unexpected-multiline': 'error', // not implemented
     // 'no-unreachable': 'error', // not implemented
     // 'no-unsafe-finally': 'error', // not implemented
-    // 'no-unsafe-negation': 'error', // not implemented
+    'no-unsafe-negation': 'error',
     // 'no-unsafe-optional-chaining': 'error', // not implemented
     // 'no-unused-labels': 'error', // not implemented
     // 'no-unused-private-class-members': 'error', // not implemented
@@ -50,7 +50,7 @@ const recommended: RslintConfigEntry = {
     // 'no-with': 'error', // not implemented
     // 'preserve-caught-error': 'error', // not implemented
     // 'require-yield': 'error', // not implemented
-    // 'use-isnan': 'error', // not implemented
+    'use-isnan': 'error',
     // 'valid-typeof': 'error', // not implemented
     'for-direction': 'error',
     'getter-return': 'error',
