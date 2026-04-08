59fe8474f9c1aa3a6b56cd71492df181eb1a446e
eryue0220 <eryue0220@gmail.com>
2026-04-08 12:14:53 +0800

    remove recommended rule

--- internal/plugins/jest/recommended.go
+++ internal/plugins/jest/recommended.go
@@ -3,7 +3,6 @@ package jest
 import (
 	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/no_disabled_tests"
 	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/no_focused_tests"
-	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/prefer_to_have_length"
 	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/valid_describe_callback"
 	"github.com/web-infra-dev/rslint/internal/rule"
 )
@@ -12,7 +11,6 @@ func GetRecommendedRules() []rule.Rule {
 	return []rule.Rule{
 		no_disabled_tests.NoDisabledTestsRule,
 		no_focused_tests.NoFocusedTestsRule,
-		prefer_to_have_length.PreferToHaveLengthRule,
 		valid_describe_callback.ValidDescribeCallbackRule,
 	}
 }
