1b656991f843776bddceb9010894b66928df3b71
eryue0220 <eryue0220@gmail.com>
2026-04-08 12:39:27 +0800

    feat: add jest rule

--- internal/plugins/jest/all.go
+++ internal/plugins/jest/all.go
@@ -4,6 +4,7 @@ import (
 	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/no_disabled_tests"
 	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/no_focused_tests"
 	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/no_hooks"
+	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/prefer_todo"
 	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/valid_describe_callback"
 	"github.com/web-infra-dev/rslint/internal/rule"
 )
@@ -13,6 +14,7 @@ func GetAllRules() []rule.Rule {
 		no_disabled_tests.NoDisabledTestsRule,
 		no_focused_tests.NoFocusedTestsRule,
 		no_hooks.NoHooksRule,
+		prefer_todo.PreferTodoRule,
 		valid_describe_callback.ValidDescribeCallbackRule,
 	}
 }
--- packages/rslint-test-tools/rstest.config.mts
+++ packages/rslint-test-tools/rstest.config.mts
@@ -243,6 +243,7 @@ export default defineConfig({
     './tests/eslint-plugin-jest/rules/no-disabled-tests.test.ts',
     './tests/eslint-plugin-jest/rules/no-focused-tests.test.ts',
     './tests/eslint-plugin-jest/rules/no-hooks.test.ts',
+    './tests/eslint-plugin-jest/rules/prefer-todo.test.ts',
     './tests/eslint-plugin-jest/rules/valid-describe-callback.test.ts',
   ],
 });
