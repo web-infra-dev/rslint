0f6550b63750209c833b15d6ab8c502ac2f27806
senze.fan <151665030@qq.com>
2026-04-12 15:26:23 +0800

    feat: port rule import/newline-after-import (#646)

--- internal/plugins/import/all.go
+++ internal/plugins/import/all.go
@@ -1,6 +1,7 @@
 package import_plugin
 
 import (
+	"github.com/web-infra-dev/rslint/internal/plugins/import/rules/newline_after_import"
 	"github.com/web-infra-dev/rslint/internal/plugins/import/rules/no_self_import"
 	"github.com/web-infra-dev/rslint/internal/plugins/import/rules/no_webpack_loader_syntax"
 	"github.com/web-infra-dev/rslint/internal/rule"
@@ -8,6 +9,7 @@ import (
 
 func GetAllRules() []rule.Rule {
 	return []rule.Rule{
+		newline_after_import.NewlineAfterImportRule,
 		no_self_import.NoSelfImportRule,
 		no_webpack_loader_syntax.NoWebpackLoaderSyntax,
 	}
