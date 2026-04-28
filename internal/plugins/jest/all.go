93793fac31511bbf29afe76831f86fa96a6ecc7f
eryue0220 <eryue0220@gmail.com>
2026-04-28 18:36:02 +0800

    add jest rule

--- internal/plugins/jest/all.go
+++ internal/plugins/jest/all.go
@@ -3,6 +3,7 @@ package jest
 import (
 	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/no_alias_methods"
 	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/no_disabled_tests"
+	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/no_done_callback"
 	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/no_focused_tests"
 	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/no_hooks"
 	"github.com/web-infra-dev/rslint/internal/plugins/jest/rules/no_test_prefixes"
@@ -17,6 +18,7 @@ func GetAllRules() []rule.Rule {
 	return []rule.Rule{
 		no_alias_methods.NoAliasMethodsRule,
 		no_disabled_tests.NoDisabledTestsRule,
+		no_done_callback.NoDoneCallbackRule,
 		no_focused_tests.NoFocusedTestsRule,
 		no_hooks.NoHooksRule,
 		no_test_prefixes.NoTestPrefixesRule,
--- packages/rslint-test-tools/rstest.config.mts
+++ packages/rslint-test-tools/rstest.config.mts
@@ -352,6 +352,7 @@ export default defineConfig({
     // eslint-plugin-jest
     './tests/eslint-plugin-jest/rules/no-alias-methods.test.ts',
     './tests/eslint-plugin-jest/rules/no-disabled-tests.test.ts',
+    './tests/eslint-plugin-jest/rules/no-done-callback.test.ts',
     './tests/eslint-plugin-jest/rules/no-focused-tests.test.ts',
     './tests/eslint-plugin-jest/rules/no-hooks.test.ts',
     './tests/eslint-plugin-jest/rules/no-test-prefixes.test.ts',
