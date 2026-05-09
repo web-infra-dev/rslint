6d75135d4c952ade98bcadb558979d21e8453458
sunyiteng <sooniter@gmail.com>
2026-05-09 13:17:37 +0800

    fix(website): render lazy playground component

--- website/theme/components/Playground/index.tsx
+++ website/theme/components/Playground/index.tsx
@@ -1,21 +1,19 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 // sideEffects
-export default function Playground() { 
+export default function Playground() {
   if (import.meta.env.SSG_MD) {
     return `
 # Playground
 
 This is a rslint playground page
-`
+`;
   }
-  const [comp, setComp] = useState<React.FC<{}> | null>(null)
-  useEffect(() => { 
-    import('./Playground').then(({ default: Playground }) => { 
-      setComp(Playground);
-    })
+  const [element, setElement] = useState<React.ReactNode>(null);
+  useEffect(() => {
+    import('./Playground').then(({ default: Playground }) => {
+      setElement(<Playground />);
+    });
   }, []);
-  return <>{comp}</>
+  return <>{element}</>;
 }
-
-
