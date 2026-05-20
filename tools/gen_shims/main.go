90fa99de537a3b7064fbb78d43417d03bae762fb
fansenze <151665030@qq.com>
2026-05-20 10:48:07 +0800

    chore: sync typescript-go submodule to 879968116
    Bump typescript-go from 0a7c6b47e to 879968116 (12 commits, mostly
    declaration-emit and type-checker fixes; no LSP/API surface changes).
    
    gen_shims: upstream Checker gained a `symbolTableAliasCache
    map[symbolTableID][]*Symbol` field (from "Speed up lookupSymbolChain"),
    where `symbolTableID` is an unexported uint64. Add a `checker.symbolTableID
    -> uint64` rewrite alongside the existing `thisAssignmentDeclarationKind
    -> int32` hack so the mirrored extra_Checker struct still compiles.

--- tools/gen_shims/main.go
+++ tools/gen_shims/main.go
@@ -325,10 +325,11 @@ func main() {
 								}
 							}
 
-							shimBuilder.WriteString(
-								// TODO: move to extra-shim.json
-								strings.ReplaceAll(types.TypeString(field.Type(), qualifierOnlyPackageName), "checker.thisAssignmentDeclarationKind", "int32"),
-							)
+							// TODO: move to extra-shim.json
+							s := types.TypeString(field.Type(), qualifierOnlyPackageName)
+							s = strings.ReplaceAll(s, "checker.thisAssignmentDeclarationKind", "int32")
+							s = strings.ReplaceAll(s, "checker.symbolTableID", "uint64")
+							shimBuilder.WriteString(s)
 						}
 						shimBuilder.WriteString("\n}\n")
 
