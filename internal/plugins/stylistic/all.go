ab769c948160f47477d3d7d33109dc20f21bf873
fansenze <151665030@qq.com>
2026-05-21 16:49:07 +0800

    feat: port rule @stylistic/arrow-spacing

--- internal/plugins/stylistic/all.go
+++ internal/plugins/stylistic/all.go
@@ -3,6 +3,7 @@ package stylistic_plugin
 import (
 	"github.com/web-infra-dev/rslint/internal/plugins/stylistic/rules/array_bracket_spacing"
 	"github.com/web-infra-dev/rslint/internal/plugins/stylistic/rules/arrow_parens"
+	"github.com/web-infra-dev/rslint/internal/plugins/stylistic/rules/arrow_spacing"
 	"github.com/web-infra-dev/rslint/internal/rule"
 )
 
@@ -10,5 +11,6 @@ func GetAllRules() []rule.Rule {
 	return []rule.Rule{
 		array_bracket_spacing.ArrayBracketSpacingRule,
 		arrow_parens.ArrowParensRule,
+		arrow_spacing.ArrowSpacingRule,
 	}
 }
