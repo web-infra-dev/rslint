1bf3ac36eb68abcef0a6c886c6ccec2b835441c3
eryue0220 <eryue0220@gmail.com>
2026-05-26 15:23:41 +0800

    add jest rule

---
+++ internal/plugins/jest/rules/prefer_called_with/prefer_called_with.go
@@ -0,0 +1,54 @@
+package prefer_called_with
+
+import (
+	"slices"
+
+	"github.com/microsoft/typescript-go/shim/ast"
+	jestUtils "github.com/web-infra-dev/rslint/internal/plugins/jest/utils"
+	"github.com/web-infra-dev/rslint/internal/rule"
+)
+
+var preferCalledWithMatcherNames = map[string]bool{
+	"toBeCalled":       true,
+	"toHaveBeenCalled": true,
+}
+
+// Message builder
+
+func buildPreferCalledWithMessage(matcherName string) rule.RuleMessage {
+	return rule.RuleMessage{
+		Id:          "preferCalledWith",
+		Description: "Prefer " + matcherName + "With(/* expected args */)",
+		Data: map[string]string{
+			"matcherName": matcherName,
+		},
+	}
+}
+
+var PreferCalledWithRule = rule.Rule{
+	Name: "jest/prefer-called-with",
+	Run: func(ctx rule.RuleContext, options any) rule.RuleListeners {
+		return rule.RuleListeners{
+			ast.KindCallExpression: func(node *ast.Node) {
+				jestFnCall := jestUtils.ParseJestFnCall(node, ctx)
+				if jestFnCall == nil ||
+					jestFnCall.Kind != jestUtils.JestFnTypeExpect ||
+					slices.Contains(jestFnCall.Modifiers, "not") {
+					return
+				}
+
+				matcherName := jestFnCall.Matcher
+				if !preferCalledWithMatcherNames[matcherName] {
+					return
+				}
+
+				reportNode := node
+				if jestFnCall.MatcherEntry != nil && jestFnCall.MatcherEntry.Node != nil {
+					reportNode = jestFnCall.MatcherEntry.Node
+				}
+
+				ctx.ReportNode(reportNode, buildPreferCalledWithMessage(matcherName))
+			},
+		}
+	},
+}
