10e975f4d5a6de6aeffe0ae47c151bbcd1cb1c11
fansenze <151665030@qq.com>
2026-07-04 15:16:46 +0800

    feat: port rule unicorn/prefer-array-flat-map

--- internal/plugins/unicorn/all.go
+++ internal/plugins/unicorn/all.go
@@ -4,6 +4,7 @@ import (
 	"github.com/web-infra-dev/rslint/internal/plugins/unicorn/rules/filename_case"
 	"github.com/web-infra-dev/rslint/internal/plugins/unicorn/rules/no_static_only_class"
 	"github.com/web-infra-dev/rslint/internal/plugins/unicorn/rules/no_useless_switch_case"
+	"github.com/web-infra-dev/rslint/internal/plugins/unicorn/rules/prefer_array_flat_map"
 	"github.com/web-infra-dev/rslint/internal/rule"
 )
 
@@ -12,5 +13,6 @@ func GetAllRules() []rule.Rule {
 		filename_case.FilenameCaseRule,
 		no_static_only_class.NoStaticOnlyClassRule,
 		no_useless_switch_case.NoUselessSwitchCaseRule,
+		prefer_array_flat_map.PreferArrayFlatMapRule,
 	}
 }
