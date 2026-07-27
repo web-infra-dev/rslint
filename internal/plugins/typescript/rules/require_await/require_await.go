e2820ff4bd07c863f77c1bc19cb91aba0d48e7b2
fansenze <151665030@qq.com>
2026-07-27 16:26:23 +0800

    perf: optimize require-await rule

--- internal/plugins/typescript/rules/require_await/require_await.go
+++ internal/plugins/typescript/rules/require_await/require_await.go
@@ -26,30 +26,110 @@ type scopeInfo struct {
 	hasAwait      bool
 	isAsyncYield  bool
 	functionFlags ast.FunctionFlags
-	upper         *scopeInfo
+}
+
+func hasCallSignature(typeChecker *checker.Checker, t *checker.Type) bool {
+	if utils.IsUnionType(t) {
+		for _, typePart := range t.Types() {
+			if len(utils.GetCallSignatures(typeChecker, typePart)) != 0 {
+				return true
+			}
+		}
+		return false
+	}
+	return len(utils.GetCallSignatures(typeChecker, t)) != 0
+}
+
+func isCallbackParameter(typeChecker *checker.Checker, param *ast.Symbol, node *ast.Node) bool {
+	t := checker.Checker_getApparentType(typeChecker, typeChecker.GetTypeOfSymbolAtLocation(param, node))
+	if param.ValueDeclaration != nil &&
+		ast.IsParameterDeclaration(param.ValueDeclaration) &&
+		param.ValueDeclaration.AsParameterDeclaration().DotDotDotToken != nil {
+		t = checker.Checker_getIndexTypeOfType(typeChecker, t, checker.Checker_numberType(typeChecker))
+		if t == nil {
+			return false
+		}
+	}
+	return hasCallSignature(typeChecker, t)
+}
+
+func hasThenCallbackSignature(typeChecker *checker.Checker, node *ast.Node, t *checker.Type) bool {
+	if utils.IsUnionType(t) {
+		for _, typePart := range t.Types() {
+			if hasThenCallbackSignature(typeChecker, node, typePart) {
+				return true
+			}
+		}
+		return false
+	}
+	for _, signature := range utils.GetCallSignatures(typeChecker, t) {
+		parameters := checker.Signature_parameters(signature)
+		if len(parameters) != 0 && isCallbackParameter(typeChecker, parameters[0], node) {
+			return true
+		}
+	}
+	return false
+}
+
+func isThenableTypePart(typeChecker *checker.Checker, node *ast.Node, t *checker.Type) bool {
+	then := checker.Checker_getPropertyOfType(typeChecker, t, "then")
+	if then == nil {
+		return false
+	}
+	return hasThenCallbackSignature(typeChecker, node, typeChecker.GetTypeOfSymbolAtLocation(then, node))
+}
+
+// isThenableType mirrors utils.IsThenableType without materializing singleton
+// slices for the overwhelmingly common non-union path.
+func isThenableType(typeChecker *checker.Checker, node *ast.Node, t *checker.Type) bool {
+	if t == nil {
+		t = typeChecker.GetTypeAtLocation(node)
+	}
+	t = checker.Checker_getApparentType(typeChecker, t)
+	if utils.IsUnionType(t) {
+		for _, typePart := range t.Types() {
+			if isThenableTypePart(typeChecker, node, typePart) {
+				return true
+			}
+		}
+		return false
+	}
+	return isThenableTypePart(typeChecker, node, t)
+}
+
+func hasAsyncIterator(typeChecker *checker.Checker, t *checker.Type) bool {
+	if utils.IsTypeFlagSet(t, checker.TypeFlagsUnionOrIntersection) {
+		for _, typePart := range t.Types() {
+			if hasAsyncIterator(typeChecker, typePart) {
+				return true
+			}
+		}
+		return false
+	}
+	return utils.GetWellKnownSymbolPropertyOfType(t, "asyncIterator", typeChecker) != nil
 }
 
 var RequireAwaitRule = rule.CreateRule(rule.Rule{
 	Name:             "require-await",
 	RequiresTypeInfo: true,
 	Run: func(ctx rule.RuleContext, options []any) rule.RuleListeners {
-		var currentScope *scopeInfo
+		var scopes []scopeInfo
 
 		enterFunction := func(node *ast.Node) {
-			currentScope = &scopeInfo{
+			scope := scopeInfo{
 				hasAwait:      false,
 				isAsyncYield:  false,
 				functionFlags: ast.FunctionFlagsNormal,
-				upper:         currentScope,
 			}
-
 			if utils.HasNonEmptyFunctionBody(node) {
-				currentScope.functionFlags = ast.GetFunctionFlags(node)
+				scope.functionFlags = ast.GetFunctionFlags(node)
 			}
+			scopes = append(scopes, scope)
 		}
 
 		exitFunction := func(node *ast.Node) {
-			if currentScope.functionFlags&ast.FunctionFlagsAsync != 0 && !currentScope.hasAwait && (currentScope.functionFlags&ast.FunctionFlagsGenerator == 0 || !currentScope.isAsyncYield) {
+			scope := &scopes[len(scopes)-1]
+			if scope.functionFlags&ast.FunctionFlagsAsync != 0 && !scope.hasAwait && (scope.functionFlags&ast.FunctionFlagsGenerator == 0 || !scope.isAsyncYield) {
 				// TODO(port): implement suggestions
 				// // If the function belongs to a method definition or
 				// // property, then the function's range may not include the
@@ -178,13 +258,26 @@ var RequireAwaitRule = rule.CreateRule(rule.Rule{
 				ctx.ReportNode(node, buildMissingAwaitMessage())
 			}
 
-			currentScope = currentScope.upper
+			scopes = scopes[:len(scopes)-1]
 		}
 
 		markAsHasAwait := func() {
-			if currentScope != nil {
-				currentScope.hasAwait = true
+			if len(scopes) != 0 {
+				scopes[len(scopes)-1].hasAwait = true
 			}
+		}
+
+		exitArrowFunction := func(node *ast.Node) {
+			scope := &scopes[len(scopes)-1]
+			if scope.functionFlags&ast.FunctionFlagsAsync != 0 && !scope.hasAwait {
+				body := ast.SkipParentheses(node.Body())
+				if !ast.IsBlock(body) &&
+					!ast.IsAwaitExpression(body) &&
+					isThenableType(ctx.TypeChecker, body, ctx.TypeChecker.GetTypeAtLocation(body)) {
+					scope.hasAwait = true
+				}
+			}
+			exitFunction(node)
 		}
 
 		return rule.RuleListeners{
@@ -201,24 +294,8 @@ var RequireAwaitRule = rule.CreateRule(rule.Rule{
 			rule.ListenerOnExit(ast.KindSetAccessor):         exitFunction,
 			ast.KindFunctionExpression:                       enterFunction,
 			rule.ListenerOnExit(ast.KindFunctionExpression):  exitFunction,
-			ast.KindArrowFunction: func(node *ast.Node) {
-				enterFunction(node)
-				// check body-less async arrow function.
-				// ignore `async () => await foo` because it's obviously correct
-				if currentScope.functionFlags&ast.FunctionFlagsAsync == 0 {
-					return
-				}
-
-				body := ast.SkipParentheses(node.Body())
-				if ast.IsBlock(body) || ast.IsAwaitExpression(body) {
-					return
-				}
-
-				if utils.IsThenableType(ctx.TypeChecker, body, ctx.TypeChecker.GetTypeAtLocation(body)) {
-					markAsHasAwait()
-				}
-			},
-			rule.ListenerOnExit(ast.KindArrowFunction): exitFunction,
+			ast.KindArrowFunction:                            enterFunction,
+			rule.ListenerOnExit(ast.KindArrowFunction):       exitArrowFunction,
 
 			ast.KindAwaitExpression: func(node *ast.Node) { markAsHasAwait() },
 			ast.KindForOfStatement: func(node *ast.Node) {
@@ -237,12 +314,16 @@ var RequireAwaitRule = rule.CreateRule(rule.Rule{
 			 *    or
 			 *  2) yields thenable type
 			 */
-			ast.KindYieldExpression: func(node *ast.Node) {
-				if currentScope == nil || currentScope.isAsyncYield {
+			rule.ListenerOnExit(ast.KindYieldExpression): func(node *ast.Node) {
+				if len(scopes) == 0 {
 					return
 				}
+				scope := &scopes[len(scopes)-1]
 				argument := node.Expression()
-				if currentScope.functionFlags&ast.FunctionFlagsGenerator == 0 || argument == nil {
+				if scope.hasAwait ||
+					scope.isAsyncYield ||
+					scope.functionFlags&ast.FunctionFlagsAsyncGenerator != ast.FunctionFlagsAsyncGenerator ||
+					argument == nil {
 					return
 				}
 
@@ -253,28 +334,29 @@ var RequireAwaitRule = rule.CreateRule(rule.Rule{
 				}
 
 				if node.AsYieldExpression().AsteriskToken == nil {
-					if utils.IsThenableType(ctx.TypeChecker, argument, ctx.TypeChecker.GetTypeAtLocation(argument)) {
-						currentScope.isAsyncYield = true
+					if isThenableType(ctx.TypeChecker, argument, ctx.TypeChecker.GetTypeAtLocation(argument)) {
+						scope.isAsyncYield = true
 					}
 					return
 				}
 
 				t := ctx.TypeChecker.GetTypeAtLocation(argument)
-				hasAsyncYield := utils.TypeRecurser(t, func(t *checker.Type) bool {
-					return utils.GetWellKnownSymbolPropertyOfType(t, "asyncIterator", ctx.TypeChecker) != nil
-				})
-				if hasAsyncYield {
-					currentScope.isAsyncYield = true
+				if hasAsyncIterator(ctx.TypeChecker, t) {
+					scope.isAsyncYield = true
 				}
 			},
-			ast.KindReturnStatement: func(node *ast.Node) {
-				if currentScope == nil || currentScope.hasAwait || currentScope.functionFlags&ast.FunctionFlagsAsync == 0 {
+			rule.ListenerOnExit(ast.KindReturnStatement): func(node *ast.Node) {
+				if len(scopes) == 0 {
+					return
+				}
+				scope := &scopes[len(scopes)-1]
+				if scope.hasAwait || scope.functionFlags&ast.FunctionFlagsAsync == 0 {
 					return
 				}
 
 				expr := node.Expression()
-				if expr != nil && utils.IsThenableType(ctx.TypeChecker, expr, ctx.TypeChecker.GetTypeAtLocation(expr)) {
-					markAsHasAwait()
+				if expr != nil && isThenableType(ctx.TypeChecker, expr, ctx.TypeChecker.GetTypeAtLocation(expr)) {
+					scope.hasAwait = true
 				}
 			},
 		}
