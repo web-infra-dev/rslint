4d61aca446b275150bb2933e9554f6049b6b4de5
fansenze <151665030@qq.com>
2026-07-28 14:12:39 +0800

    perf: optimize default-case-last traversal

--- internal/rules/default_case_last/default_case_last.go
+++ internal/rules/default_case_last/default_case_last.go
@@ -10,39 +10,34 @@ var DefaultCaseLastRule = rule.Rule{
 	Name: "default-case-last",
 	Run: func(ctx rule.RuleContext, options []any) rule.RuleListeners {
 		return rule.RuleListeners{
-			ast.KindSwitchStatement: func(node *ast.Node) {
-				switchStmt := node.AsSwitchStatement()
-				if switchStmt == nil {
+			ast.KindDefaultClause: func(node *ast.Node) {
+				parent := node.Parent
+				if parent == nil || parent.Kind != ast.KindCaseBlock {
 					return
 				}
 
-				caseBlockNode := switchStmt.CaseBlock
-				if caseBlockNode == nil {
-					return
-				}
-
-				caseBlock := caseBlockNode.AsCaseBlock()
+				caseBlock := parent.AsCaseBlock()
 				if caseBlock == nil || caseBlock.Clauses == nil {
 					return
 				}
 
-				clauseNodes := caseBlock.Clauses.Nodes
-				if len(clauseNodes) == 0 {
+				clauses := caseBlock.Clauses.Nodes
+				if len(clauses) == 0 || clauses[len(clauses)-1] == node {
 					return
 				}
 
-				// Find the default clause
-				for i, clause := range clauseNodes {
-					if clause.Kind == ast.KindDefaultClause {
-						// If it's not the last clause, report
-						if i != len(clauseNodes)-1 {
-							ctx.ReportNode(clause, rule.RuleMessage{
-								Id:          "notLast",
-								Description: "Default clause should be the last clause.",
-							})
-						}
+				for _, clause := range clauses {
+					if clause.Kind != ast.KindDefaultClause {
+						continue
+					}
+					if clause != node {
 						return
 					}
+					ctx.ReportNode(node, rule.RuleMessage{
+						Id:          "notLast",
+						Description: "Default clause should be the last clause.",
+					})
+					return
 				}
 			},
 		}
