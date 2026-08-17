6eb062f890668d1a52cb25969cd5a9873a03cd79
Elecmonkey <elecmonkey@hotmail.com>
2026-08-17 10:47:31 +0800

    feat(rstest): add no-focused-tests rule (#1363)

--- internal/plugins/rstest/all.go
+++ internal/plugins/rstest/all.go
@@ -5,6 +5,7 @@ import (
 	"github.com/web-infra-dev/rslint/internal/plugins/rstest/rules/no_commented_out_tests"
 	"github.com/web-infra-dev/rslint/internal/plugins/rstest/rules/no_conditional_expect"
 	"github.com/web-infra-dev/rslint/internal/plugins/rstest/rules/no_disabled_tests"
+	"github.com/web-infra-dev/rslint/internal/plugins/rstest/rules/no_focused_tests"
 	"github.com/web-infra-dev/rslint/internal/plugins/rstest/rules/no_identical_title"
 	"github.com/web-infra-dev/rslint/internal/plugins/rstest/rules/no_interpolation_in_snapshots"
 	"github.com/web-infra-dev/rslint/internal/plugins/rstest/rules/no_mocks_import"
@@ -18,6 +19,7 @@ func GetAllRules() []rule.Rule {
 		no_commented_out_tests.NoCommentedOutTestsRule,
 		no_conditional_expect.NoConditionalExpectRule,
 		no_disabled_tests.NoDisabledTestsRule,
+		no_focused_tests.NoFocusedTestsRule,
 		no_identical_title.NoIdenticalTitleRule,
 		no_interpolation_in_snapshots.NoInterpolationInSnapshotsRule,
 		no_mocks_import.NoMocksImportRule,
