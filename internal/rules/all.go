d2e4bdf7d2ea583dea0dcf23cb7be5f144515ba6
swwind <i@sww.moe>
2026-08-20 13:06:14 +0800

    feat: port rule func-names (#1738)

--- internal/rules/all.go
+++ internal/rules/all.go
@@ -15,6 +15,7 @@ import (
 	"github.com/web-infra-dev/rslint/internal/rules/dot_notation"
 	"github.com/web-infra-dev/rslint/internal/rules/eqeqeq"
 	"github.com/web-infra-dev/rslint/internal/rules/for_direction"
+	"github.com/web-infra-dev/rslint/internal/rules/func_names"
 	"github.com/web-infra-dev/rslint/internal/rules/getter_return"
 	"github.com/web-infra-dev/rslint/internal/rules/guard_for_in"
 	"github.com/web-infra-dev/rslint/internal/rules/init_declarations"
@@ -188,6 +189,7 @@ func GetAllRules() []rule.Rule {
 		default_case_last.DefaultCaseLastRule,
 		dot_notation.DotNotationRule,
 		for_direction.ForDirectionRule,
+		func_names.FuncNamesRule,
 		getter_return.GetterReturnRule,
 		guard_for_in.GuardForInRule,
 		init_declarations.InitDeclarationsRule,
