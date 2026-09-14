ab40583e42b963aff59aa07d9536859ef2f33d2f
swwind <i@sww.moe>
2026-09-14 16:53:25 +0800

    feat(react): add jsx-curly-newline rule (#2147)

--- internal/plugins/react/all.go
+++ internal/plugins/react/all.go
@@ -88,6 +88,7 @@ import (
 
 	"github.com/web-infra-dev/rslint/internal/plugins/react/rules/jsx_child_element_spacing"
 	"github.com/web-infra-dev/rslint/internal/plugins/react/rules/jsx_curly_brace_presence"
+	"github.com/web-infra-dev/rslint/internal/plugins/react/rules/jsx_curly_newline"
 	"github.com/web-infra-dev/rslint/internal/plugins/react/rules/jsx_curly_spacing"
 	"github.com/web-infra-dev/rslint/internal/plugins/react/rules/jsx_no_comment_textnodes"
 	"github.com/web-infra-dev/rslint/internal/plugins/react/rules/jsx_no_leaked_render"
@@ -183,6 +184,7 @@ func GetAllRules() []rule.Rule {
 		void_dom_elements_no_children.VoidDomElementsNoChildrenRule,
 		jsx_child_element_spacing.JsxChildElementSpacingRule,
 		jsx_curly_brace_presence.JsxCurlyBracePresenceRule,
+		jsx_curly_newline.JsxCurlyNewlineRule,
 		jsx_curly_spacing.JsxCurlySpacingRule,
 		jsx_no_comment_textnodes.JsxNoCommentTextnodesRule,
 		jsx_no_leaked_render.JsxNoLeakedRenderRule,
