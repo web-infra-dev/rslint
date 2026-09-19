8ae2a95aabc45aa364a17cce85924ebf76e103be
Elecmonkey <elecmonkey@hotmail.com>
2026-09-20 02:03:03 +0800

    test(unicorn): remove redundant preset assertion

--- packages/rslint-test-tools/tests/cli/js-config/presets.test.ts
+++ packages/rslint-test-tools/tests/cli/js-config/presets.test.ts
@@ -182,7 +182,6 @@ describe('defineConfig and config presets', () => {
     expect(rec.rules?.['unicorn/no-array-fill-with-reference-type']).toBe(
       'error',
     );
-    expect(rec.rules?.['unicorn/no-zero-fractions']).toBe('error');
     expect(rec.rules?.['unicorn/no-useless-error-capture-stack-trace']).toBe(
       'error',
     );
