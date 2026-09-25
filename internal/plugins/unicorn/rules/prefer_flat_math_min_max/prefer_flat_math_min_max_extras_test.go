248912ff171fb58d7f99ced7f95a3e278060ab49
Hashem Khalifa <hashem.khalifa@gmail.com>
2026-09-25 22:01:01 +0200

    style(unicorn): gofmt edit-demand imports

--- internal/plugins/unicorn/rules/prefer_flat_math_min_max/prefer_flat_math_min_max_extras_test.go
+++ internal/plugins/unicorn/rules/prefer_flat_math_min_max/prefer_flat_math_min_max_extras_test.go
@@ -6,9 +6,9 @@ import (
 
 	"github.com/microsoft/TypeScript/tsc/shim/ast"
 	"github.com/web-infra-dev/rslint/internal/linter"
-	lintprogram "github.com/web-infra-dev/rslint/internal/program"
 	"github.com/web-infra-dev/rslint/internal/plugins/unicorn/fixtures"
 	"github.com/web-infra-dev/rslint/internal/plugins/unicorn/rules/prefer_flat_math_min_max"
+	lintprogram "github.com/web-infra-dev/rslint/internal/program"
 	"github.com/web-infra-dev/rslint/internal/rule"
 	"github.com/web-infra-dev/rslint/internal/rule_tester"
 )
