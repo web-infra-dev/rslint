324e069d67ecb96ae11278d1aedaf780ba333006
Hashem Khalifa <hashem.khalifa@gmail.com>
2026-09-25 21:59:10 +0200

    fix(unicorn): use program package in last-method edit-demand test

--- internal/plugins/unicorn/rules/prefer_array_last_methods/prefer_array_last_methods_extras_test.go
+++ internal/plugins/unicorn/rules/prefer_array_last_methods/prefer_array_last_methods_extras_test.go
@@ -6,7 +6,7 @@ import (
 
 	"github.com/microsoft/TypeScript/tsc/shim/ast"
 	"github.com/web-infra-dev/rslint/internal/linter"
-	"github.com/web-infra-dev/rslint/internal/lintprogram"
+	lintprogram "github.com/web-infra-dev/rslint/internal/program"
 	"github.com/web-infra-dev/rslint/internal/plugins/unicorn/fixtures"
 	"github.com/web-infra-dev/rslint/internal/plugins/unicorn/rules/prefer_array_last_methods"
 	"github.com/web-infra-dev/rslint/internal/rule"
